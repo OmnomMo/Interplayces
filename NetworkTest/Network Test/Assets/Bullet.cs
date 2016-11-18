3f7af961036c13995f5759c61dffaa508768224b
Moritz <m-ruehrlinger@gmx.at>
2016-11-18 11:46:13 +0100

    Added networkTest Project

---
+++ NetworkTest/Network Test/Assets/Bullet.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+
+        Debug.Log("Collision!");
+
+
+        if ((hit.GetComponent<Health>() as Health) != null) {
+
+            Debug.Log("damage!");
+
+            var health = hit.GetComponent<Health>();
+            if (health != null)
+            {
+                health.TakeDamage(10);
+            }
+
+        }
+
+        Destroy(gameObject);
+    }
+}

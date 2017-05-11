46b22855946f7c6872fc2cc61cd569ab05336ba9
Kathrin Kefer <kathkefer@gmail.com>
2017-05-11 09:18:19 +0200

    Added HoloLens Networking Test (Server)

---
+++ HololensNetworkingTest/Assets/Message.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization;
+using System.IO;
+
+[DataContract() ]
+public class Message : System.Object {
+
+	[DataMember()]
+	public int commandID;
+	[DataMember()]
+	public string[] parameters;
+
+}

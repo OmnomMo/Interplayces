bdb02b96fa91ec52e26c8b68a868f600ae2c5eb6
OmnomMo <m-ruehrlinger@gmx.at>
2017-11-06 09:42:19 +0100

    Soundtrack I

---
+++ NetworkTest/Network Test/Assets/LevelMusic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMusic : MonoBehaviour {
+
+    public AudioClip levelMusic;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
